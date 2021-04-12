b81784ee9902561a25411ab57c61101f964b7795
Lazi_Algorithm <ikponmwobaanderson@gmail.com>
2021-04-12 03:09:58 +0100

    Added class based views for List and Details

--- base/todo_list/urls.py
+++ base/todo_list/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import TaskList, TaskDetail
 
 
 urlpatterns = [
-    path('', views.index, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name = 'task')
 ]
--- base/todo_list/views.py
+++ base/todo_list/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("TODO LIST")
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todo_list/task.html'
